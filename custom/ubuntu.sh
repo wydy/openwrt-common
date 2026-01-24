b055f2f2d149ca6dcc51e00144e5921c99e26a3f
Wydy <1937986+wydy@users.noreply.github.com>
2026-01-24 23:52:39 +0800

    Update package installation commands in ubuntu.sh

--- custom/ubuntu.sh
+++ custom/ubuntu.sh
@@ -4,14 +4,16 @@ PWD_DIR="$(pwd)"
 
 function install_mustrelyon(){
 echo -e "\033[36m开始升级ubuntu插件和安装依赖.....\033[0m"
+
+echo "::group::使用apt安装必要软件包"
 # 更新ubuntu源
-apt-get update -y
+apt update -y
 
 # 升级ubuntu
-apt-get full-upgrade -y
+apt full-upgrade -y
 
 # 安装编译openwrt的依赖
-apt-get install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
   bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib \
   g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev \
   libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev \
@@ -19,6 +21,7 @@ apt-get install -y ack antlr3 asciidoc autoconf automake autopoint binutils biso
   ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils \
   python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs \
   upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd
+echo "::endgroup::"
 
 cd $PWD_DIR
 
